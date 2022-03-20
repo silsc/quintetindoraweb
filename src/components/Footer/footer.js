d50a5e95b51b5369bd71b59c8c06b89c26342abc
Silvia Salat <salat.silvia@gmail.com>
2022-03-20 12:16:07 +0100

    ADD(youtube footer link)

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,9 +1,10 @@
 import "./footer.scss"
 import * as React from "react"
 import PropTypes from "prop-types"
-import InstaIcon from "../../images/insta.svg";
-import FbIcon from "../../images/fb.svg";
-import EmailIcon from "../../images/email.svg";
+import InstaIcon from "../../images/insta.svg"
+import FbIcon from "../../images/fb.svg"
+import EmailIcon from "../../images/email.svg"
+import YoutubeIcon from "../../images/youtube.svg"
 import { Link } from "gatsby"
 
 const Footer = ({ siteTitle }) => (
@@ -12,50 +13,67 @@ const Footer = ({ siteTitle }) => (
       <h5>xarxes</h5>
       <ul className="footer-links">
         <li className="footer-link">
-          <a href="https://www.instagram.com/indoraquintet/"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{
-                  textDecoration: `none`,
-                }}
-          >
-                <InstaIcon />
-          </a>
-        </li>
-        <li className="footer-link">
-          <a href="https://www.facebook.com/indoraquintet"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  style={{
-                    textDecoration: `none`,
-                  }}
-            >
-                  <FbIcon />
-          </a>
-        </li>
-        <li className="footer-link">
-          <a href='mailto:quintetindora@gmail.com'
+          <a
+            href="https://www.instagram.com/indoraquintet/"
             target="_blank"
             rel="noopener noreferrer"
             style={{
               textDecoration: `none`,
-            }}>
-              <EmailIcon />
+            }}
+          >
+            <InstaIcon />
+          </a>
+        </li>
+        <li className="footer-link">
+          <a
+            href="https://www.youtube.com/channel/UC4tKCOt21fXYnSsgdH7b3qg"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              textDecoration: `none`,
+            }}
+          >
+            <YoutubeIcon />
+          </a>
+        </li>
+        <li className="footer-link">
+          <a
+            href="https://www.facebook.com/indoraquintet"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              textDecoration: `none`,
+            }}
+          >
+            <FbIcon />
+          </a>
+        </li>
+        <li className="footer-link">
+          <a
+            href="mailto:quintetindora@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              textDecoration: `none`,
+            }}
+          >
+            <EmailIcon />
           </a>
         </li>
       </ul>
-      <p className="footer-credits">Designed and developed by  
-          <Link
-                to="https://www.heyitssilvia.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="silsc"
-                style={{
-                  textDecoration: `none`,
-                }}
-          >
-                {" Silvia Salat"} 
-          </Link>
+      <p className="footer-credits">
+        Designed and developed by
+        <Link
+          to="https://www.heyitssilvia.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="silsc"
+          style={{
+            textDecoration: `none`,
+          }}
+        >
+          {" Silvia Salat"}
+        </Link>
       </p>
     </div>
   </footer>
