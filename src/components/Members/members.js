140db36bc8f5bf253e0b13acb86aed2a7f090a69
Silvia Salat <salat.silvia@gmail.com>
2022-03-20 12:16:52 +0100

    CHANGE(members position)

--- src/components/Members/members.js
+++ src/components/Members/members.js
@@ -9,19 +9,32 @@ import natalia from "../../images/natalia.jpg"
 import roger from "../../images/roger.jpg"
 
 const Members = () => (
-    <section className="members" id="members">
-        <p className="page-number">03</p>
-        <h2 className="members-title">Membres</h2>
-        <div className="members-container">
-          <MemberCard name="Daniel Folqué" role="Contratenor" imgid={daniel}></MemberCard>
-          <MemberCard name="Neus Llorens" role="Soprano" imgid={neus} girl="true"></MemberCard>
-          <MemberCard name="Marc Garcia" role="Tenor" imgid={marc}></MemberCard>
-          <MemberCard name="Natàlia Sánchez" role="Soprano" imgid={natalia} girl="true"></MemberCard>
-          <MemberCard name="Roger Casanova" role="Baix" imgid={roger}></MemberCard>
-        </div>
-    </section>
+  <section className="members" id="members">
+    <p className="page-number">03</p>
+    <h2 className="members-title">Membres</h2>
+    <div className="members-container">
+      <MemberCard name="Roger Casanova" role="Baix" imgid={roger}></MemberCard>
+      <MemberCard
+        name="Neus Llorens"
+        role="Soprano"
+        imgid={neus}
+        girl="true"
+      ></MemberCard>
+      <MemberCard name="Marc Garcia" role="Tenor" imgid={marc}></MemberCard>
+      <MemberCard
+        name="Natàlia Sánchez"
+        role="Soprano"
+        imgid={natalia}
+        girl="true"
+      ></MemberCard>
+      <MemberCard
+        name="Daniel Folqué"
+        role="Contratenor"
+        imgid={daniel}
+      ></MemberCard>
+    </div>
+  </section>
 )
- 
 
 Members.propTypes = {
   siteTitle: PropTypes.string,
